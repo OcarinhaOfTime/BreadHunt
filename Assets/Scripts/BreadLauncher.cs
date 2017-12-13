6332fa12b8290434fc410f4909be5ff1fed18f87
Vagner Seibert <vagnerseibert@gmail.com>
2017-12-13 14:35:52 -0200

    Added BreadLauncher

---
+++ Assets/Scripts/BreadLauncher.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreadLauncher : MonoBehaviour {
+    public Rigidbody bread;
+    private Camera mainCam;
+
+    public float verticalForce = 10;
+    public float horizontalForce = 10;
+
+    void Start () {
+        mainCam = Camera.main;
+
+    }
+	
+	void Update () {
+        if (Input.GetButtonDown("Jump")) {
+            LaunchBread();
+        }
+	}
+
+    private void LaunchBread() {
+        var breadInst = Instantiate(bread);
+        breadInst.transform.position = transform.position;
+        breadInst.gameObject.SetActive(true);
+        breadInst.AddForce(Vector3.up * verticalForce - mainCam.transform.right * horizontalForce, ForceMode.Impulse);
+    }
+}
